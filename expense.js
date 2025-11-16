675e85b0d4e3fa61e33ba730dad2a7d1fed48783
DHighLord <sibnamohsin@gmail.com>
2025-11-16 12:21:49 +0900

    Complete expense.js

--- expense.js
+++ expense.js
@@ -1,19 +1,29 @@
 import fs from "fs";
-import { get } from "http";
 import path from "path"
 
 const filePath = path.resolve("./expenses.json");
 
 function loadExpenses() {
+    if (!fs.existsSync(filePath))
+        return [];
 
+    const data = fs.readFileSync(filePath, "utf-8");
+
+    try {
+        return JSON.parse(data);
+    } catch {
+        console.error(" Corrupted data file. Resetting...");
+        fs.writeFileSync(filePath, "[]");
+        return [];
+    }
 }
 
 function saveExpenses(expenses) {
-
+    fs.writeFileSync(filePath, JSON.stringify(expenses, null, 2));
 }
 
 function getNextId(expenses) {
-
+    return expenses.length === 0 ? 1 : Math.max(...expenses.map(e => e.id)) + 1;
 }
 
 export function addExpense(description, amount) {
@@ -31,17 +41,72 @@ export function addExpense(description, amount) {
 }
 
 export function updateExpense(id, description, amount) {
+    const expenses = loadExpenses();
+    const idx = expenses.findIndex(e => e.id === id);
 
+    if (idx === -1) {
+        console.error("Expense not found.");
+        return;
+    }
+
+    if (description)
+        expenses[idx].description = description;
+
+    if (!isNaN(amount))
+        expenses[idx].amount = amount;
+
+    expenses[idx].updatedAt = new Date().toISOString();
+    saveExpenses(expenses);
+    console.log(`Updated expense ${id}`);
 }
 
 export function deleteExpense(id) {
+    let expense = loadExpenses();
+    const len = expense.length;
+    expenses = expenses.filter(e => e.id !== id);
 
+    if (expenses === len) {
+        console.error("Expense not found.");
+        return;
+    }
+
+    saveExpenses(expenses);
+    console.log(`Deleted expense ${id}`);
 }
 
 export function listExpense() {
+    const expenses = loadExpenses();
 
+    if (expenses.length === 0) {
+        console.log("No expenses found.");
+        return;
+    }
+
+    console.log("Expense List: ");
+    expenses.forEach(e =>
+        console.log(`[${e.id}] ${e.description} - $${e.amount} (${e.date})`)
+    );
 }
 
 export function summary(month = null) {
-    
+    const expenses = loadExpenses();
+
+    if (expenses.length === 0) {
+        console.log("No expenses found.");
+        return;
+    }
+
+    let filtered = expenses;
+
+    if (month) {
+        filtered = expenses.filter(e => new Date(e.date).getMonth() + 1 === month);
+    }
+
+    const total = filtered.reduce((sum, e) => sum + e.amount, 0);
+    if (month)
+        console.log(`Summary for month ${month}:`);
+    else
+        console.log("Overall summary:");
+    console.log(`Total expenses: $${total}`);
+    console.log(`Number of entries: ${filtered.length}`);
 }
